4182ded54158c17d528c728ef5dfded3b30dcb98
rubenacevedo3 <raceved3@umd.edu>
2017-10-14 18:36:10 -0700

    I created the motionModuleStub test for task 8. I also deleted test.cpp since it did not write that code

---
+++ test/motionModuleStubTest.cpp
@@ -0,0 +1,27 @@
+/**
+ *@author Ruben Acevedo
+ *@file motionModuleStubTest.cpp
+ *@brief This is the ".cpp" file for testing the motionModuleStub Class
+ *@copyright [2017] Ruben Acevedo
+ *
+ * This file tests the motionModuleStub Class using google test
+ *
+ */
+#include <motionModuleStub.hpp>
+#include <gtest/gtest.h>
+
+  //! test for walk
+  /**
+   * This test to make sure when the walk command is entered
+   * the robot walks
+   *
+   */
+TEST(PIDControllerTest, walkTest) {
+  motionModuleStub m;
+
+
+}
+
+//TEST(PIDControllerTest, Test2) {
+
+//}
