48d7121a3dd96769a1ae6501e3bfc1942790df04
rubenacevedo3 <raceved3@umd.edu>
2017-10-15 09:57:34 -0700

    I created the test cases for the command class for task 11. I had to modify the CMake list to compile the test. I also notice some error in the commets of the motionModuleStubTest.cpp so I fixed them.

---
+++ test/commandTest.cpp
@@ -0,0 +1,69 @@
+/**
+ *@author Ruben Acevedo
+ *@file commandTest.cpp
+ *@brief This is the ".cpp" file for testing the command Class
+ *@copyright [2017] Ruben Acevedo
+ *
+ * This file tests the command Class using google test
+ *
+ */
+#include <command.hpp>
+#include <gtest/gtest.h>
+#include <string>
+
+//! test the class constructor audioString setting
+/**
+ * This tests to make sure that when the class is created
+ * the audioString is set to "!@#$%". At the same time it
+ * is also testing the getAudioString function.
+ *
+ */
+TEST(commandTest, constructorAudioStringTest) {
+  command c("walk");
+  auto s = c.getAudioString();
+  EXPECT_EQ("!@#$%", s);
+}
+
+//! test the class constructor commandName setting
+/**
+ * This tests to make sure that when the class is created
+ * the commandName is set to whatever the action string
+ * is that was inputed in the constructor argument. At the
+ * same time it is also testing the getCommandName function.
+ *
+ */
+TEST(commandTest, constructorCommandNameTest) {
+  command c("walk");
+  auto s = c.getCommandName();
+  EXPECT_EQ("walk", s);
+}
+
+//! test the setAudioString function
+/**
+ * This tests to make sure that when the audioString is being set
+ * it is actually being saved in the class' audioString.
+ *
+ */
+TEST(commandTest, setAudioStringTest) {
+  command c("lie down");
+  auto as = "RoboDog play dead";
+  c.setAudioString(as);
+  auto s = c.getAudioString();
+  EXPECT_EQ(as, s);
+}
+
+//! test to make sure the commandName does not ever change
+/**
+ * This tests to make sure that when when any function is called
+ * the commandName will always stay the same.
+ *
+ */
+TEST(commandTest, noChangeCommandNameTest) {
+  command c("chase tail");
+  auto st = c.getAudioString();
+  auto as = "RoboDog play dead";
+  c.setAudioString(as);
+  auto s = c.getAudioString();
+  auto cmd = c.getCommandName();
+  EXPECT_EQ("chase tail", cmd);
+}
--- test/motionModuleStubTest.cpp
+++ test/motionModuleStubTest.cpp
@@ -11,90 +11,90 @@
 #include <gtest/gtest.h>
 #include <string>
 
-  //! test for walk
-  /**
-   * This test to make sure that when the walk command is entered
-   * the robot walks
-   *
-   */
-TEST(PIDControllerTest, walkTest) {
+//! test for walk
+/**
+ * This tests to make sure that when the walk command is entered
+ * the robot walks
+ *
+ */
+TEST(motionModuleStubTest, walkTest) {
   motionModuleStub m;
   EXPECT_EQ("**RoboDog walks**", m.executeMotion("walk"));
 }
 
 //! test for turning right
 /**
- * This test to make sure that when the turn right command is entered
+ * This tests to make sure that when the turn right command is entered
  * the robot turns right
  *
  */
-TEST(PIDControllerTest, turnRightTest) {
+TEST(motionModuleStubTest, turnRightTest) {
 motionModuleStub m;
 EXPECT_EQ("**RoboDog turns right**", m.executeMotion("turn right"));
 }
 
 //! test for turning left
 /**
- * This test to make sure that when the turn left command is entered
+ * This tests to make sure that when the turn left command is entered
  * the robot turns left
  *
  */
-TEST(PIDControllerTest, turnLeftTest) {
+TEST(motionModuleStubTest, turnLeftTest) {
 motionModuleStub m;
 EXPECT_EQ("**RoboDog turns left**", m.executeMotion("turn left"));
 }
 
 //! test for stopping
 /**
- * This test to make sure that when the stop command is entered
+ * This tests to make sure that when the stop command is entered
  * the robot stops moving
  *
  */
-TEST(PIDControllerTest, stopTest) {
+TEST(motionModuleStubTest, stopTest) {
 motionModuleStub m;
 EXPECT_EQ("**RoboDog stops moving**", m.executeMotion("stop"));
 }
 
 //! test for sitting
 /**
- * This test to make sure that when the sit command is entered
+ * This tests to make sure that when the sit command is entered
  * the robot sits
  *
  */
-TEST(PIDControllerTest, sitTest) {
+TEST(motionModuleStubTest, sitTest) {
 motionModuleStub m;
 EXPECT_EQ("**RoboDog sits**", m.executeMotion("sit"));
 }
 
 //! test for sitting
 /**
- * This test to make sure that when the sit command is entered
+ * This tests to make sure that when the sit command is entered
  * the robot sits
  *
  */
-TEST(PIDControllerTest, lieDownTest) {
+TEST(motionModuleStubTest, lieDownTest) {
 motionModuleStub m;
 EXPECT_EQ("**RoboDog lies down**", m.executeMotion("lie down"));
 }
 
 //! test for sitting
 /**
- * This test to make sure that when the lie down command is entered
+ * This tests to make sure that when the lie down command is entered
  * the robot lies down
  *
  */
-TEST(PIDControllerTest, chaseTailTest) {
+TEST(motionModuleStubTest, chaseTailTest) {
 motionModuleStub m;
 EXPECT_EQ("**RoboDog chases its tail**", m.executeMotion("chase tail"));
 }
 
 //! test for bad command
 /**
- * This test to make sure that when a command is entered
+ * This tests to make sure that when a command is entered
  * incorrectly the robot does nothing
  *
  */
-TEST(PIDControllerTest, badCommandTest) {
+TEST(motionModuleStubTest, badCommandTest) {
 motionModuleStub m;
 EXPECT_EQ("**RoboDog does nothing**", m.executeMotion("chase Tail"));
 }
