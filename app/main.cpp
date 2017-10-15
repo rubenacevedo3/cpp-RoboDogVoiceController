70e7e4d5dc0854fd08d2b85f371936ddaa1c3bc6
rubenacevedo3 <31897376+rubenacevedo3@users.noreply.github.com>
2017-10-14 20:24:09 -0400

    Update main.cpp
    I update it to quickly test my motionModule stub

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,23 @@
+/**
+ *@author Ruben Acevedo
+ *@file main.cpp
+ *@brief This is the main.cpp file for the whole RoboDogVoiceController-eclipse project
+ *@copyright [2017] Ruben Acevedo
+ *
+ * This file will run the an example demo that
+ * simulates full functionality of the RoboDog
+ * Voice Controller component
+ *
+ */
+
 #include <iostream>
-#include <lib.hpp>
+#include <string>
+#include <motionModuleStub.hpp>
 
 int main()
 {
-    dummy();
+    motionModuleStub m;
+    std::string cmd = "walk";
+    m.executeMotion(cmd);
     return 0;
 }
