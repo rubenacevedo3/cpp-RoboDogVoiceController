20f55449b2b0a0ecbe9eb10cd73f525d83c625c7
rubenacevedo3 <raceved3@umd.edu>
2017-10-16 21:14:56 -0700

    Task 22: I coded up the Demo. The whole code complies with minimal errors in cppcheck and cpplint

--- app/main.cpp
+++ app/main.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <string>
 
+
   //! the main function
   /**
    * This function is the int main() that runs the demo that
@@ -23,9 +24,111 @@
    *@return an integer
    */
 int main() {
+  /**
+   * Set up the demo
+   */
+  std::cout << "---------------------------"
+    "-----------------------Starting "
+    "the Demo-----------------------"
+    "---------------------------" << std::endl;
+  std::cout << "***" << std::endl;
+  std::cout << "***" << std::endl;
+  std::cout << "***" << std::endl;
   voiceControlInterface vci;
-  vci.turnOn();
-  vci.trainMode(false);
-  vci.listen(false);
-  vci.turnOff();
+
+  vci.turnOn(); /*!< turns the voiceContoller Interface on */
+  std::cout << "***" << std::endl;
+  std::cout << "***" << std::endl;
+  std::cout << "***" << std::endl;
+  std::string in;
+  bool off = false;
+
+  while (vci.isOn()) { /*!< does loop until user turns Interface off */
+	  /**
+	   * Sets up the main menu
+	   */
+    std::cout << "---------------------------"
+      "---------------------------Main "
+      "Menu---------------------------"
+      "---------------------------" << std::endl;
+    std::cout << "Select the mode you want:" << std::endl;
+
+     /**
+      * promts the user to select a mode
+      */
+    std::cout << "Enter 't' for training mode, enter 'l'"
+      " for listening mode, or enter 'x' to turn the Voice"
+      " Interface off: " << std::endl;
+    std::getline(std::cin, in);
+
+    /**
+     * does training mode
+     */
+    if (in == "t") {
+      vci.trainMode(false);
+
+      /**
+       * promts the user to turn the Interface off
+       */
+      std::cout << "Do you want to turn the Voice "
+        "Interface off (y/n)? :" << std::endl;
+      std::getline(std::cin, in);
+      /**
+       * turns the Interface off
+       */
+      if (in == "y") {
+        std::cout << "***" << std::endl;
+        std::cout << "***" << std::endl;
+        std::cout << "***" << std::endl;
+        vci.turnOff();
+        std::cout << "***" << std::endl;
+        std::cout << "***" << std::endl;
+        std::cout << "***" << std::endl;
+        std::cout << "Demo is over." << std::endl;
+      }
+      /**
+       * does listening mode
+       */
+    } else if (in == "l") {
+        vci.listen(false);
+        /**
+         * promts the user to turn the Interface off
+         */
+        std::cout << "Do you want to turn the Voice "
+          "Interface off (y/n)? :" << std::endl;
+        std::getline(std::cin, in);
+        /**
+          * turns the Interface off
+          */
+        if (in == "y") {
+          std::cout << "***" << std::endl;
+          std::cout << "***" << std::endl;
+          std::cout << "***" << std::endl;
+          vci.turnOff();
+          std::cout << "***" << std::endl;
+          std::cout << "***" << std::endl;
+          std::cout << "***" << std::endl;
+          std::cout << "Demo is over." << std::endl;
+        }
+        /**
+          * turns the Interface off
+          */
+    } else if (in == "x") {
+        std::cout << "***" << std::endl;
+        std::cout << "***" << std::endl;
+        std::cout << "***" << std::endl;
+        vci.turnOff();
+        std::cout << "***" << std::endl;
+        std::cout << "***" << std::endl;
+        std::cout << "***" << std::endl;
+        std::cout << "Demo is over." << std::endl;
+        /**
+          * User puts an invalid input
+          * it prompts the user to try again
+          */
+    } else {
+        std::cout << "!!!!!!!!Error: Try again!!!!!!!!!" << std::endl;
+      }
+  }
+  return 0;
 }
